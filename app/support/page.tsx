58ce0e81eb10ce8c18222305b900d6266d1c9aaa
Matthew Talma <matthew.talma@gmail.com>
2025-08-27 15:49:24 -0700

    Disable ESLint rule for unescaped entities in SupportPage to allow for specific character usage.

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react/no-unescaped-entities */
 
 import React, { useState } from "react";
 import { Flame, ArrowLeft, Mail, MessageCircle, Shield, Clock, ChevronRight } from "lucide-react";
